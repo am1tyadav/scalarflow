2eb48c0c74867e5ed73681209b70ad97dc662419
Amit Yadav <amit.yadav.iitr@gmail.com>
2022-12-01 12:24:26 +0000

    updated python version

--- setup.py
+++ setup.py
@@ -24,5 +24,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.10.8",
+    python_requires=">=3.11.0",
 )
